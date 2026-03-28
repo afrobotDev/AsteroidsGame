8fe4e8094cfd024c4c4159ecd6d4fc878a72d62b
YitbarekE <yitbarekendale.2024@gmail.com>
2026-03-28 14:44:02 +0300

    feat: add group class

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) 
     clock = pygame.time.Clock()
     dt = 0
-    
+
     while True:
         log_state() 
         for event in pygame.event.get():
@@ -17,10 +21,13 @@ def main():
                 return
         
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
 
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
 
