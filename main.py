dd8d892a2a513d1bff778fc09c62d23ed86c3020
YitbarekE <yitbarekendale.2024@gmail.com>
2026-03-28 09:41:59 +0300

    feat: add delta time

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -11,7 +13,8 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
