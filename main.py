670de999d2a3d62487d7f98aa42bcd865eb5717a
YitbarekE <yitbarekendale.2024@gmail.com>
2026-03-28 12:28:53 +0300

    feat: move left and right

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player 
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) 
     clock = pygame.time.Clock()
     dt = 0
+    
     while True:
-        log_state()
+        log_state() 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = clock.tick(60) / 1000
+        player.update(dt)
+
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)/1000
-        print(dt)
+
+
 
 if __name__ == "__main__":
     main()
